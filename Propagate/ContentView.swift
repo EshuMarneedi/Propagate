fd2617a3adfb7137d56087f314c5161f4c6b4a3d
Eshu Marneedi <emarneedi@gmail.com>
2026-02-25 23:51:28 -0500

    Initial Commit

---
+++ Propagate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Propagate
+//
+//  Created by Eshu Marneedi on 2/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Propagate/PropagateApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropagateApp.swift
+//  Propagate
+//
+//  Created by Eshu Marneedi on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct PropagateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
